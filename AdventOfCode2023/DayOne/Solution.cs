173b54792d47a0435eb38cc58308e9cfde9a43da
Chris Williams <chris.kwilliams@hotmail.com>
2023-12-01 18:54:12 +0200

    Initial solution commit - solve day one

---
+++ AdventOfCode2023/DayOne/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.DayOne
+{
+    internal class Solution
+    {
+        public static void QuestionOne()
+        {
+            var regex = @"\d";
+
+            Console.WriteLine(Solve(regex));
+        }
+
+        public static void QuestionTwo()
+        {
+            var regex = @"\d|one|two|three|four|five|six|seven|eight|nine";
+
+            Console.WriteLine(Solve(regex));
+        }
+
+        private static int Solve(string regex)
+        {
+            var lines = File.ReadAllLines(@"DayOne\data.txt");
+            int answer = 0;
+
+            foreach (var line in lines)
+            {
+                var first = Regex.Match(line, regex).Value;
+                var last = Regex.Match(line, regex, RegexOptions.RightToLeft).Value;
+                var final = WordToNumber(first) + WordToNumber(last);
+                answer += int.Parse(final);
+            }
+
+            return answer;
+        }
+
+        private static string WordToNumber(string s)
+        {
+            return s.ToLower() switch
+            {
+                "one" => "1",
+                "two" => "2",
+                "three" => "3",
+                "four" => "4",
+                "five" => "5",
+                "six" => "6",
+                "seven" => "7",
+                "eight" => "8",
+                "nine" => "9",
+                _ => s,
+            };
+        }
+    }
+}
---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day one part one:");
+            DayOne.Solution.QuestionOne();
+
+            Console.WriteLine("Day two part two:");
+            DayOne.Solution.QuestionTwo();
+        }
+    }
+}
